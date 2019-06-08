dd42f29122ede859d8137770de95c05bea3b1926
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2019-06-08 19:49:07 +0900

    描画領域のコピー時にpython関数をかませた

--- head.js
+++ head.js
@@ -4,20 +4,11 @@ function exec(){
 
 function hook(e){
     function callback(x){
-        image2.src = x.content.data['text/plain'];
+        document.getElementById('result').src = x.content.data['text/plain'].split("'")[1];
     };
     var src = canvas.toDataURL();
-
     var funcname = 'generate';
     kernel.execute(`${funcname}('${src}')`, {'iopub': {"output": callback}}, {silent:false});
-};
-
-function python(funcname, src){
-    function callback(x){
-        dst = x.content.data['text/plain'];
-    };
-    kernel.execute(`${funcname}('${src}')`, {'iopub': {"output": callback}}, {silent:false});
-    return dst;
 };
 
 //touchstart touchmove touchend
@@ -28,11 +19,4 @@ function loaded(){
     canvas.addEventListener("touchend", hook);
 
     fence = document.getElementById('fence').contentWindow;
-
-    canvas2 = document.getElementById ('result');
-    ctx2 = canvas2.getContext ('2d');
-    image2 = new Image();
-    image2.onload = function(){
-        ctx2.drawImage(image2, 0,0,400,400)
-    }
 };
